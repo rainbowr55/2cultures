46b9c1a0dca5bab298b1e5bc4755f39747d07bc4
sdkongkong <sdkongkong@sina.com>
2016-11-11 15:53:15 +1100

    login

--- twocultures/app/src/main/java/com/twoculture/twoculture/ui/LaunchActivity.java
+++ twocultures/app/src/main/java/com/twoculture/twoculture/ui/LaunchActivity.java
@@ -1,15 +1,47 @@
 package com.twoculture.twoculture.ui;
 
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.twoculture.twoculture.R;
 
-public class LaunchActivity extends AppCompatActivity {
+public class LaunchActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btn_login_original;
+    Button btn_login_email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        initView();
+        initListenter();
+    }
+
+    private void initView(){
+        btn_login_original = (Button) this.findViewById(R.id.btn_login_original);
+        btn_login_email = (Button) this.findViewById(R.id.btn_login_email);
+    }
+
+    private void initListenter(){
+        btn_login_original.setOnClickListener(this);
+        btn_login_email.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_login_original:
+                Intent intent = new Intent(this,LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_login_email:
+                Intent emailIntent = new Intent(this,SignupActivity.class);
+                startActivity(emailIntent);
+                break;
+        }
     }
 }
