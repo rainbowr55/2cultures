5c6a76925b4c33f9d0818186bb7695b8aaf0cbcb
sdkongkong <sdkongkong@sina.com>
2016-11-21 00:03:56 +1100

    增加facebook登陆

--- twocultures/app/src/main/java/com/twoculture/twoculture/ui/LaunchActivity.java
+++ twocultures/app/src/main/java/com/twoculture/twoculture/ui/LaunchActivity.java
@@ -3,22 +3,55 @@ package com.twoculture.twoculture.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.twoculture.twoculture.R;
 
 public class LaunchActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btn_login_original;
     Button btn_login_email;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_launch);
         initView();
         initListenter();
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) this.findViewById(R.id.btn_login_facebook);
+        loginButton.setReadPermissions("email");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.d("facebook","onSuccess");
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.d("facebook","onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.d("facebook","onError");
+            }
+        });
     }
 
     private void initView() {
@@ -43,5 +76,10 @@ public class LaunchActivity extends AppCompatActivity implements View.OnClickLis
                 startActivity(emailIntent);
                 break;
         }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
