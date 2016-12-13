2d08d908a54c5d7e04286dc3ef964f1b22950526
sdkongkong <sdkongkong@sina.com>
2016-12-13 21:09:49 +1100

    add file

---
+++ twocultures/app/src/main/java/com/twoculture/twoculture/models/Comment.java
@@ -0,0 +1,19 @@
+package com.twoculture.twoculture.models;
+
+/**
+ * Created by Kevin Song on 12/12/2016.
+ * Copyright (c) 2016 Woolworths. All rights reserved.
+ * test
+ */
+
+public class Comment {
+    public int comment_id;
+    public String created_at;
+    public boolean is_self;
+    public String text;
+    public static class User{
+        public int user_id;
+        public String user_name;
+        public String header_image;
+    }
+}
